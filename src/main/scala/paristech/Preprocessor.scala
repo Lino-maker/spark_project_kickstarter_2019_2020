945c30e3d14ee0913f5eabcc6c88159378f4efa2
Lino <fofo82a@hotmail.com>
2019-12-08 17:53:53 +0100

    commit project Spark

--- src/main/scala/paristech/Preprocessor.scala
+++ src/main/scala/paristech/Preprocessor.scala
@@ -1,7 +1,9 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 
 object Preprocessor {
 
@@ -28,6 +30,8 @@ object Preprocessor {
       .appName("TP Spark : Preprocessor")
       .getOrCreate()
 
+    import spark.implicits._
+
     /*******************************************************************************
       *
       *       TP 2
@@ -40,8 +44,115 @@ object Preprocessor {
       *
       ********************************************************************************/
 
-    println("\n")
-    println("Hello World ! from Preprocessor")
-    println("\n")
+    /***************
+      * Load data in a DataFrame
+      ******/
+
+    val df: DataFrame = spark
+      .read
+      .option("header", value = true) // utilise la première ligne du (des) fichier(s) comme header
+      .option("inferSchema", value = true) // pour inférer le type de chaque colonne (Int, String, etc.)
+      .csv("src/main/resources/train_clean.csv")
+
+    println(s"Nombre de lignes : ${df.count}")
+    println(s"Nombre de colonnes : ${df.columns.length}")
+
+    println(s"Affichage du DataFrame sous forme de tableau :")
+    df.show()
+    println(s"Affichage du shéma du DataFrame : ")
+    df.printSchema()
+
+    println(s"Assignement de type Int aux colonnes concernées :")
+    val dfCasted: DataFrame = df
+      .withColumn("goal", $"goal".cast("Int"))
+      .withColumn("deadline" , $"deadline".cast("Int"))
+      .withColumn("state_changed_at", $"state_changed_at".cast("Int"))
+      .withColumn("created_at", $"created_at".cast("Int"))
+      .withColumn("launched_at", $"launched_at".cast("Int"))
+      .withColumn("backers_count", $"backers_count".cast("Int"))
+      .withColumn("final_status", $"final_status".cast("Int"))
+
+    println(s"Affichage du shéma du DataFrame Casted : ")
+    dfCasted.printSchema()
+
+    println(s"Affichage de la description statistique des colonnes de type Int : ")
+    dfCasted
+      .select("goal", "backers_count", "final_status")
+      .describe()
+      .show
+
+    println(s"Etude des différentes colonnes en vue d'en proposer un nettoyage pertinent :")
+    dfCasted.groupBy("disable_communication").count.orderBy($"count".desc).show(100)
+
+    dfCasted.groupBy("country").count.orderBy($"count".desc).show(100)
+
+    dfCasted.groupBy("currency").count.orderBy($"count".desc).show(100)
+
+    dfCasted.select("deadline").dropDuplicates.show()
+
+    dfCasted.groupBy("state_changed_at").count.orderBy($"count".desc).show(100)
+
+    dfCasted.groupBy("backers_count").count.orderBy($"count".desc).show(100)
+
+    dfCasted.select("goal", "final_status").show(30)
+
+    dfCasted.groupBy("country", "currency").count.orderBy($"count".desc).show(50)
+
+    println(s"On retire la colonne disable_communication en créant un nouveau DataFrame :")
+    val df2: DataFrame = dfCasted.drop("disable_communication")
+
+    df2.printSchema()
+
+    println(s"On retire les fuites du futur :")
+    val dfNoFutur: DataFrame = df2.drop("backers_count", "state_changed_at")
+
+    dfNoFutur.printSchema()
+
+    //df.filter($"country" === "False")
+      //.groupBy("currency")
+      //.count
+      //.orderBy($"count".desc)
+      //.show(50)
+
+    //Après avoir implémenté des udf sur notre Jupyter Notebook pour s'y exercer, on privilégie ici les fonctions de Spark
+    println(s"En utilisant les fonctions de Spark, on nettoie les colonnes currency et country : ")
+    val dfCountry: DataFrame = dfNoFutur
+      .withColumn("country2", when($"country" === "False", $"currency").otherwise($"country"))
+      .withColumn("currency2", when($"country".isNotNull && length($"currency") =!= 3, null).otherwise($"currency"))
+      .drop("country", "currency")
+
+    //Changement des dates avec to_timestamp
+    val dfTest2 : DataFrame = dfCountry
+      .withColumn("deadline", to_timestamp($"deadline"))
+      .withColumn("launched_at", to_timestamp($"launched_at"))
+      .withColumn("created_at", to_timestamp($"created_at"))
+
+    //Création des colonnes days_campaign et hours_prepa pour connaître les durées précises
+    val dfTest3 : DataFrame = dfTest2
+      .withColumn("days_campaign", datediff($"deadline",$"launched_at"))
+      .withColumn("hours_prepa", unix_timestamp($"launched_at")-unix_timestamp($"created_at"))
+
+    //Ultimes nettoyages des colonnes avec remplacement des valeurs Null, changement de la casse et conservation uniquement
+    //des lignes où le final_status est correctement rempli afin de faciliter la classification à venir.
+    val dfTest4 : DataFrame = dfTest3
+      .withColumn("name", lower($"name"))
+      .withColumn("desc", lower($"desc"))
+      .withColumn("keywords",lower($"keywords"))
+      .withColumn("text",concat_ws(" ",$"name",$"desc",$"keywords"))
+      .withColumn("hours_prepa", round($"hours_prepa"/3600,3))
+      .withColumn("hours_prepa",when($"hours_prepa".isNull, -1).otherwise($"hours_prepa"))
+      .withColumn("days_campaign",when($"days_campaign".isNull, -1).otherwise($"days_campaign"))
+      .withColumn("goal",when($"goal".isNull, -1).otherwise($"goal"))
+      .withColumn("country2",when($"country2".isNull, "unknown").otherwise($"country2"))
+      .withColumn("currency2",when($"currency2".isNull, "unknown").otherwise($"currency2"))
+      .filter($"final_status"===0 || $"final_status" ===1)
+      .drop("name","desc","keywords","deadline","created_at","launched_at")
+
+    println(s"DataFrame nettoyé:")
+    dfTest4.show()
+
+    //Export du Dataframe ainsi créé.
+    dfTest4.write.mode("overwrite").parquet("src/main/resources/dfTest4")
+
   }
 }
--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -1,7 +1,15 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{CountVectorizer, IDF, OneHotEncoderEstimator, RegexTokenizer, StopWordsRemover, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.mutable.ArrayBuffer
+
 
 
 object Trainer {
@@ -42,5 +50,131 @@ object Trainer {
 
     println("hello world ! from Trainer")
 
+    val df = spark.read.parquet("src/main/resources/dfTest4")
+
+    println("STAGE 1")
+    val tokenizer = new RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+
+    //val wordsData : DataFrame = tokenizer.transform(df)
+
+    println("STAGE 2")
+    val stopWordRemover = new StopWordsRemover()
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("filtered")
+      .setStopWords(Array("the","a","http","i","me","to","what","in","rt"))
+
+    //val wordRemover : DataFrame = stopWordRemover.transform(wordsData)
+
+    println("STAGE 3")
+    val countVectorizer = new CountVectorizer()
+      .setInputCol(stopWordRemover.getOutputCol)
+      .setOutputCol("tf")
+
+    //val dfTest = countVectorizer.fit(wordRemover)
+
+    println("STAGE 4")
+    val idf = new IDF().setInputCol(countVectorizer.getOutputCol).setOutputCol("tfidf")
+
+    println("STAGE 5")
+    val indexercountry = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+
+    println("STAGE 6")
+    val indexercurrency = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+
+    println("STAGE 7 et 8")
+    val encoder = new OneHotEncoderEstimator()
+      .setInputCols(Array(indexercountry.getOutputCol, indexercurrency.getOutputCol))
+      .setOutputCols(Array("country_onehot", "currency_onehot"))
+
+    println("STAGE 9")
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal",
+        "country_onehot", "currency_onehot"))
+      .setOutputCol("features")
+
+    println("STAGE 10")
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(20)
+
+    println(s"Création du pipeline :")
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, stopWordRemover, countVectorizer, idf, indexercountry,
+        indexercurrency, encoder, assembler, lr))
+
+    val sets = df.randomSplit(Array[Double](0.9,0.1))
+    val training = sets(0)
+    val test = sets(1)
+
+    val model1 = pipeline.fit(training)
+
+    println(s"Entraînement et sauvegarde du modèle 1 sans réglage des hyper-paramètres : ")
+    println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
+
+    val dfWithSimplePredictions = model1.transform(test)
+
+    println(s"Affichage des résultats du modèle 1 : ")
+    dfWithSimplePredictions.groupBy("final_status", "predictions").count.show()
+
+
+    val evaluator = new MulticlassClassificationEvaluator().setLabelCol("final_status").setPredictionCol("predictions")
+    val f1SimpleScore = evaluator.evaluate(dfWithSimplePredictions)
+    println(f1SimpleScore)
+
+    println(s"A présent, réglage des hyperparamètres et création d'un Cross-Validated modèle (cvModel) :")
+    val elNetCvValues = getLogScale(1e-8, 10, 2)
+    val minDFCvValues = Array(55.0, 75.0, 95.0)
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(lr.regParam, elNetCvValues)
+      .addGrid(countVectorizer.minDF, minDFCvValues)
+      .build()
+
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(new MulticlassClassificationEvaluator().setLabelCol("final_status").setPredictionCol("predictions"))
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.7)
+      .setSeed(18)
+
+    val cvModel = trainValidationSplit.fit(training)
+    val dfWithPredictions = cvModel.transform(test)
+    println(s"Affichage des résultats obtenus avec réglage des hyperparamètres :")
+    dfWithPredictions.groupBy("final_status", "predictions").count.show()
+    val f1SimpleScoreCV = cvModel.getEvaluator.evaluate(dfWithPredictions)
+    println(f1SimpleScoreCV)
+
+
+  }
+  private def getLogScale(from : Double, to : Double, logStep : Double):Array[Double] = {
+    val initArray = new ArrayBuffer[Double]()
+    initArray += from
+    incrementLogScale(initArray, to, logStep).toArray
+  }
+
+  @scala.annotation.tailrec
+  private def incrementLogScale(current : ArrayBuffer[Double], to : Double, logStep : Double):ArrayBuffer[Double] = {
+    if (current.last < to){
+      current += current.last*math.pow(10, logStep)
+      incrementLogScale(current, to, logStep)
+    }
+    else{
+      current
+    }
   }
 }
